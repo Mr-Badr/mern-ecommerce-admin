cc82691d6e7ba949a6f11cd4a396544c2391842d
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-03-01 05:51:46 +0100

    add some pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Resetpassword from "./pages/Resetpassword";
 import Forgotpassword from "./pages/Forgotpassword";
 import MainLayout from "./components/MainLayout";
 import Enquiries from "./pages/Enquiries";
+import Bloglist from "./pages/Bloglist";
+import Blogcatlist from "./pages/Blogcatlist";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import Colorlist from "./pages/Colotlist";
+import Categorylist from "./pages/Categorylist";
 
 function App() {
   return (
@@ -17,6 +23,12 @@ function App() {
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="enquiries" element={<Enquiries />} />
+          <Route path="blog-list" element={<Bloglist />} />
+          <Route path="blog-category-list" element={<Blogcatlist />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="list-color" element={<Colorlist />} />
+          <Route path="list-category" element={<Categorylist />} />
         </Route>
       </Routes>
     </Router>
