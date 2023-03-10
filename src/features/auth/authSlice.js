a4bf96f3d43e64423c1a07fc88bfdc2a39d41bcc
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-03-10 16:00:33 +0100

    update Login auth

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,17 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "./authServices";
 
-const userDefaultState = {
-  _id: null,
-  firstname: null,
-  lastname: null,
-  email: null,
-  mobile: null,
-  tokens: null,
-}
+const getUserfromLocalStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
 
 const initialState = {
-  user: userDefaultState,
+  user: getUserfromLocalStorage,
   orders: [],
   isError: false,
   isLoading: false,
