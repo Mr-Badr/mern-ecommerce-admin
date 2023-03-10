8655314e95ec62b560bd38ada4c84b7b194cebd5
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-03-10 17:33:05 +0100

    Customers feature

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-
+import customerReducer from "../features/cutomers/customerSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-
+    customer: customerReducer,
   },
 });
